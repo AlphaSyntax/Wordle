3ebdc235f204c48340f26d7fd77412378f93d7f3
AlphaSyntax <mamabolobashimane@gmail.com>
2024-04-11 20:55:44 +0200

    Game logic done

--- wyrdle.py
+++ wyrdle.py
@@ -13,3 +13,7 @@ for guess_num in range(1, 7):
                        if (letter == correct)}
     misplaced_letters = (set(guess) & set(WORD) - correct_letters)
     wrong_letters = set(guess) - set(WORD)
+
+    print("Correct letters:",", ".join(sorted(correct_letters)))
+    print("Misplaced letters:",", ".join(sorted(misplaced_letters)))
+    print("Wrong letters:",", ".join(sorted(wrong_letters)))
