a60fe25b0cbc9683e12eea5a6d81833153ad89dc
AlphaSyntax <mamabolobashimane@gmail.com>
2024-04-11 21:10:31 +0200

    Can read word list and choose secret word

--- wyrdle.py
+++ wyrdle.py
@@ -1,18 +1,24 @@
+import pathlib
+import random
 
-WORD = "PYTHON"
+# Choosing random secret word
+WORDLIST = pathlib.Path("wordlist.txt")
+words = [ word.upper() for word in WORDLIST.read_text(encoding="uft-8").strip().split("\n")]
+word = random.choice(words)
+
 # Ask the user six times t guess the word
 for guess_num in range(1, 7):
     guess = input(f"\nGuess {guess_num}: ").upper()
-    if (guess == WORD):
+    if (guess == word):
         print("Correct")
         break
 
 # user guess is "snake" and secret word is "PYTHON"
 # correct letter, misplaced letter, wrong letter
-    correct_letters = {letter for letter, correct in zip(guess, WORD)
+    correct_letters = {letter for letter, correct in zip(guess, word)
                        if (letter == correct)}
-    misplaced_letters = (set(guess) & set(WORD) - correct_letters)
-    wrong_letters = set(guess) - set(WORD)
+    misplaced_letters = (set(guess) & set(word) - correct_letters)
+    wrong_letters = (set(guess) - set(word))
 
     print("Correct letters:",", ".join(sorted(correct_letters)))
     print("Misplaced letters:",", ".join(sorted(misplaced_letters)))
