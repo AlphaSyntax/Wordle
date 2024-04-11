750088c73905eb6de0e9854c73559ce0ca51e70a
AlphaSyntax <mamabolobashimane@gmail.com>
2024-04-11 22:37:00 +0200

    finished refactoring

--- wyrdle.py
+++ wyrdle.py
@@ -3,52 +3,48 @@ import random
 from string import ascii_letters
 
 
-
-# Choosing random secret word
-WORDLIST = pathlib.Path("wordlist.txt")
-
-
-words = [ 
-        word.upper() 
-        for word in WORDLIST.read_text(encoding="uft-8").split("\n")
-        if (len(word) == 5) and all(letter in ascii_letters for letter in word)
-        ]
-word = random.choice(words)
-
-
-# Ask the user six times t guess the word
-for guess_num in range(1, 7):
-    guess = input(f"\nGuess {guess_num}: ").upper()
-    if (guess == word):
-        print("Correct")
-        break
-
-# user guess is "snake" and secret word is "PYTHON"
-# correct letter, misplaced letter, wrong letter
-    correct_letters = {letter for letter, correct in zip(guess, word)
-                       if (letter == correct)
-                       }
-    misplaced_letters = (set(guess) & set(word) - correct_letters)
-    wrong_letters = (set(guess) - set(word))
-
-    print("Correct letters:",", ".join(sorted(correct_letters)))
-    print("Misplaced letters:",", ".join(sorted(misplaced_letters)))
-    print("Wrong letters:",", ".join(sorted(wrong_letters)))
-else:
-    print(f"The word was {word}")
-
-
 def main():
     # Pre-process
     word = get_random_word()
 
     # Process (main loop)
     for guess_num in range(1, 7):
-        guess = input(f"Guess {guess_num}: ").upper()
+        guess = input(f"\nGuess {guess_num}: ").upper()
 
-        show_guess()
+        show_guess(guess, word)
         if (guess == word):
             break
+
     # Post-process
     else:
-        game_over()
+        game_over(word)
+
+
+def get_random_word():
+    wordlist = pathlib.Path(__file__).parent / "wordlist.txt"
+    words = [
+        word.upper()
+        for word in wordlist.read_text(encoding="utf-8").split("\n")
+        if (len(word) == 5) and all((letter in ascii_letters) for letter in word)
+    ]
+    return random.choice(words)
+
+
+def show_guess(guess, word):
+    correct_letters = {
+                        letter for letter, correct in zip(guess, word) if letter == correct
+                      }
+    misplaced_letters = (set(guess) & set(word)) - correct_letters
+    wrong_letters = set(guess) - set(word)
+
+    print("Correct letters:", ", ".join(sorted(correct_letters)))
+    print("Misplaced letters:", ", ".join(sorted(misplaced_letters)))
+    print("Wrong letters:", ", ".join(sorted(wrong_letters)))
+
+
+def game_over(word):
+    print(f"The word was {word}")
+
+
+if __name__ == "__main__":
+    main()
