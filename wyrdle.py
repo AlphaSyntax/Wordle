482f692f399d6fd673010778a2334567ba00b89d
AlphaSyntax <mamabolobashimane@gmail.com>
2024-04-11 20:38:58 +0200

    Ask the user 6 times to guess the word

--- wyrdle.py
+++ wyrdle.py
@@ -1,5 +1,9 @@
-guess = input("Guess a word: ").upper()
-if (guess == "PYTHON"):
-    print("Correct")
-else:
+
+# Ask the user six times t guess the word
+for guess_num in range(1, 7):
+    guess = input(f"\nGuess {guess_num}: ").upper()
+    if (guess == "PYTHON"):
+        print("Correct")
+        break
+
     print("Wrong")
